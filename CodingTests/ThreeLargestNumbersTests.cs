078ceac177230a2aa3a00870ef344447cf0a2012
Culai Chiritoiu <c.chiritoiu07@gmail.com>
2020-08-06 14:53:13 +0300

    ThreeLargestNumbers

---
+++ CodingTests/ThreeLargestNumbersTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using ThreeLargestNumbers;
+
+namespace CodingTests
+{
+    public class ThreeLargestNumbersTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void ThreeLargestNumbers_ShouldReturnFirstThreeGreatest()
+        {
+            //Arrange
+            ThreeLargestNo tln = new ThreeLargestNo();
+
+            //Act
+            ulong[] result = tln.FindThreeLargestNumbers(new ulong[] { 141, 1, 17, 4, 3, 12, 18, 541, 8, 1, 2 });
+
+            //Assert
+            Assert.That(result, Is.EqualTo(new ulong[] { 18, 141, 541 }));
+        }
+    }
+}
---
+++ ThreeLargestNumbers/ThreeLargestNo.cs
@@ -0,0 +1,50 @@
+﻿namespace ThreeLargestNumbers
+{
+    public class ThreeLargestNo
+    {
+        /// <summary>
+        /// Given a shuffled array of numbers, find the three greatest numbers
+        /// Order the largest numbers ascending
+        /// [141, 1, 17, 4, 3, 12, 18, 541, 8, 1, 2]
+        /// </summary>
+        /// <param name="array"></param>
+        /// O(n) time | O(1) space
+        public ulong[] FindThreeLargestNumbers(ulong[] array)
+        {
+            ulong[] largestNumbers = new ulong[3] { ulong.MinValue, ulong.MinValue, ulong.MinValue };
+            foreach (ulong number in array)
+            {
+                FindLargest(number, largestNumbers, 2);
+            }
+            return largestNumbers;
+        }
+
+        private void FindLargest(ulong number, ulong[] largestNumbers, int index)
+        {
+            for (int i = index; i >= 0; i--)
+            {
+                if (largestNumbers[i] < number)
+                {
+                    ShiftAndUpdate(largestNumbers, number, i);
+                    break;
+                }
+            }
+        }
+
+        private void ShiftAndUpdate(ulong[] largestNumbers, ulong number, int index)
+        {
+            for (int i = 0; i <= largestNumbers.Length - 1; i++)
+            {
+                if (i == index)
+                {
+                    largestNumbers[i] = number;
+                    break;
+                }
+                else
+                {
+                    largestNumbers[i] = largestNumbers[i + 1];
+                }
+            }
+        }
+    }
+}
