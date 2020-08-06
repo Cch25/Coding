7c43d65328a0d1897d3e96cf3e5aecf9d7246ba8
Culai Chiritoiu <c.chiritoiu07@gmail.com>
2020-08-06 14:08:11 +0300

    ProdSum

---
+++ CodingTests/ProductSumTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using ProductSum;
+
+namespace CodingTests
+{
+    public class ProductSumTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void ProductSum_RecursiveCall()
+        {
+            //Arrange
+            ProdSum prodSum = new ProdSum();
+            
+            //Act - [5,2[7,-1],3,[6,[-13,8],4]]
+            object[] arr = new object[] { 5, 2, new object[] { 7, -1 }, 3, new object[] { 6, new object[] { -13, 8 }, 4 } };
+            int result = prodSum.ProductSum(arr);
+           
+            //Assert
+            Assert.That(result, Is.EqualTo(12));
+        }
+    }
+}
---
+++ ProductSum/ProdSum.cs
@@ -0,0 +1,33 @@
+﻿namespace ProductSum
+{
+    public class ProdSum
+    {
+        /// <summary>
+        /// Get the product sum of the given array:
+        /// [5,2,[7,-1],3,[6,[-13,8],4]]
+        /// The subarray will be multiplied by the depth
+        /// Result should be 12
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="multiplier"></param>
+        /// <returns></returns>
+
+        public int ProductSum(object[] array, int multiplier = 1)
+        {
+            int sum = 0;
+            foreach (object element in array)
+            {
+                if (element.GetType().IsArray)
+                {
+                    sum += ProductSum(element as object[], multiplier + 1);
+                }
+                else
+                {
+                    sum += (int)element;
+                }
+            }
+            return sum * multiplier;
+
+        }
+    }
+}
