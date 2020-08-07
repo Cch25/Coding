a59c1c68e2f1fe23627846c0d3c6e43bfb2ead9b
Culai Chiritoiu <c.chiritoiu07@gmail.com>
2020-08-07 10:55:00 +0300

    Organize Questions by dificulty level

--- CodingTests/EasyTests/CaesarCipher.cs
+++ CodingTests/EasyTests/CaesarCipher.cs
@@ -8,8 +8,13 @@ namespace CodingTests
         [Test]
         public void CaesarCipher_RetunsEncryptedString()
         {
+            //Arrange
             Caesar caesar = new Caesar();
+
+            //Act
             string result = caesar.Encrypt("xyz", 2);
+
+            //Assert
             Assert.That(result, Is.EqualTo("zab"));
         }
     }
