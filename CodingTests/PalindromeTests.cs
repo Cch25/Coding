ebf9b52be1ce0cd608e6a6c286e7acccc703cb95
Culai Chiritoiu <c.chiritoiu07@gmail.com>
2020-08-07 10:01:46 +0300

    Palindrome

---
+++ CodingTests/PalindromeTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Palindrome;
+
+namespace CodingTests
+{
+    public class PalindromeTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Palindrome_TestNaive()
+        {
+            //Arrange
+            FindPalindrome fp = new FindPalindrome();
+
+            //Act
+            bool result = fp.NaivePalindrome("abcdcba");
+            bool result2 = fp.IsPalindromeSolutionTwo("abcdcba");
+            bool result3 = fp.IsPalindromeRecursive("abcdcba");
+            bool result4 = fp.IsPalindrome("abcdcba");
+            //Assert
+            Assert.That(result, Is.True);
+            Assert.That(result2, Is.True);
+            Assert.That(result3, Is.True);
+            Assert.That(result4, Is.True);
+
+        }
+    }
+}
---
+++ Palindrome/FindPalindrome.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace Palindrome
+{
+    public class FindPalindrome
+    {
+        //O(2^n) time | O(n) space
+        public bool NaivePalindrome(string word)
+        {
+            string newString = string.Empty;
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                newString += word[i];
+            }
+            return word.Equals(newString);
+        }
+
+        //O(n) time | O(n) space
+        public bool IsPalindromeSolutionTwo(string word)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                sb.Append(word[i]);
+            }
+            return word.Equals(sb.ToString());
+        }
+
+        //O(n) time | O(n) space
+        public bool IsPalindromeRecursive(string word, int left = 0)
+        {
+            int right = word.Length - 1 - left;
+            if (left > right)
+            {
+                return true;
+            }
+            if (word[left] != word[right])
+            {
+                return false;
+            }
+            return IsPalindromeRecursive(word, left + 1);
+        }
+
+        //O(n) time | O(1) space
+        public bool IsPalindrome(string word)
+        {
+            int leftPointer = 0;
+            int rightPointer = word.Length - 1;
+            while (leftPointer < rightPointer)
+            {
+                if(word[leftPointer] != word[rightPointer])
+                {
+                    return false;
+                }
+                leftPointer++;
+                rightPointer--;
+            }
+            return true;
+        }
+    }
+}
