58c3a0a9df9db04f745c69a0351d41071fbbdf74
Culai Chiritoiu <c.chiritoiu07@gmail.com>
2020-08-07 17:40:42 +0300

    fix

--- CodingTests/QuestionTests/MoveToEndTests.cs
+++ CodingTests/QuestionTests/MoveToEndTests.cs
@@ -13,7 +13,7 @@ namespace CodingTests.QuestionTests
             int[] result1 = mte.MoveElementToEndDifferentSolution(new int[] { 2, 1, 2, 2, 2, 3, 4, 2 }, 2);
 
             Assert.That(result, Is.EqualTo(new int[] { 1, 3, 4, 2, 2, 2, 2, 2 }));
-            Assert.That(result, Is.EqualTo(new int[] { 4, 1, 3, 2, 2, 2, 2, 2 }));
+            Assert.That(result1, Is.EqualTo(new int[] { 4, 1, 3, 2, 2, 2, 2, 2 }));
         }
     }
 }
