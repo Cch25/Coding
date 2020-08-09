f6b6087e234f46d1bdd30cf80079060ad07e2ce6
Culai Chiritoiu <c.chiritoiu07@gmail.com>
2020-08-09 13:19:48 +0300

    Make a change minum amount

--- CodingTests/QuestionTests/WaysToMakeChangeTests.cs
+++ CodingTests/QuestionTests/WaysToMakeChangeTests.cs
@@ -8,9 +8,27 @@ namespace CodingTests.QuestionTests
         [Test]
         public void WaysToMakeChange()
         {
+            //Arrange
             WaysToMakeAChange wmc = new WaysToMakeAChange();
+
+            //Act
             int result = wmc.WaysToChange(new int[] { 1, 5, 10, 25 }, 12);
+
+            //Assert
             Assert.That(result, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void MinNumsToMakeChange()
+        {
+            //Arrange
+            WaysToMakeAChange wmc = new WaysToMakeAChange();
+
+            //Act
+            int result = wmc.MinNumsOfCoinsToChange(new int[] { 1, 2, 4 }, 6);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(2));
         }
     }
 }
--- Questions/WaysToMakeChange/WaysToMakeAChange.cs
+++ Questions/WaysToMakeChange/WaysToMakeAChange.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 
 namespace WaysToMakeChange
 {
@@ -15,20 +14,45 @@ namespace WaysToMakeChange
         /// <returns>Number of ways</returns>
         public int WaysToChange(int[] coins, int amount)
         {
-            List<int> ways = new List<int>();
+            int[] ways = new int[amount + 1];
             for (int i = 0; i <= amount; i++)
             {
-                ways.Add(0);
+                ways[i] = 0;
             }
             ways[0] = 1;
             foreach (int coin in coins)
             {
-                for (int j = coin; j <= amount; j++)
+                for (int currAmount = coin; currAmount <= amount; currAmount++)
                 {
-                    ways[j] += ways[j - coin];
+                    ways[currAmount] += ways[currAmount - coin];
                 }
             }
             return ways[amount];
+        }
+        /// <summary>
+        /// Given an array of coins [1, 2, 4]
+        /// What's the minimum number of coins to make a change for $6?
+        /// O(nd) time | O(n)
+        /// </summary>
+        /// <param name="coins"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public int MinNumsOfCoinsToChange(int[] coins, int amount)
+        {
+            int[] numOfCoins = new int[amount + 1];
+            for (int i = 0; i <= amount; i++)
+            {
+                numOfCoins[i] = int.MaxValue;
+            }
+            numOfCoins[0] = 0;
+            foreach (int coin in coins)
+            {
+                for (int currAmount = coin; currAmount <= amount; currAmount++)
+                {
+                    numOfCoins[currAmount] = Math.Min(numOfCoins[currAmount], 1 + numOfCoins[currAmount - coin]);
+                }
+            }
+            return numOfCoins[amount] != int.MaxValue ? numOfCoins[amount] : -1;
         }
     }
 }
